857e335c9ec77737f22710995a38e17897e89c1a
kyle kersey <kylelk@comcast.net>
2016-04-06 08:32:27 -0700

    added config file

---
+++ src/config.ads
@@ -0,0 +1,12 @@
+with GNAT.Directory_Operations;
+package Config is
+   function Join(Path : String) return String is (GNAT.Directory_Operations.Format_Pathname(Path));
+
+   Data_Dir : constant String := ".note_data/";
+
+   Object_Dir : constant String := Join(Data_Dir & "objects");
+
+   Temp_Dir : constant String := Join(Data_Dir & "temp");
+
+   Branch_JSON_File : constant String := Join(Data_Dir & "branches.json");
+end Config;
--- src/main.adb
+++ src/main.adb
@@ -1,7 +1,24 @@
 with Ada.Text_IO;
+with Ada.Directories;
+with Config;
 
 procedure Main is
    package TIO renames Ada.Text_IO;
+
+   procedure Setup_Project is
+      procedure Create_Dir(Path : String) is
+      begin
+         if not Ada.Directories.Exists (Path) then
+            Ada.Directories.Create_Directory (Path);
+         end if;
+      end Create_Dir;
+   begin
+      Create_Dir(Config.Data_Dir);
+      Create_Dir(Config.Object_Dir);
+      Create_Dir(Config.Temp_Dir);
+   end Setup_Project;
+
 begin
+   Setup_Project;
    TIO.Put_Line("hello world");
 end Main;
