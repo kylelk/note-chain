1ae67836f2578f64869f3aa8158200b14d3dcd2e
kyle kersey <kylelk@comcast.net>
2016-04-07 08:27:15 -0700

    added loading and saving of branches to JSON file

--- src/main.adb
+++ src/main.adb
@@ -4,6 +4,7 @@ with GNATCOLL_JSON;
 with Ada.Strings.Unbounded;
 with Ada.Containers.Hashed_Maps;
 with Ada.Strings.Unbounded.Hash;
+with Ada.IO_Exceptions;
 
 with Config;
 with File_Operations;
@@ -15,9 +16,11 @@ procedure Main is
 
 
    subtype SHA256_Value is String(1..64);
+   Null_Sha256 : constant SHA256_Value :=
+     "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855";
    type Branch is record
       Name : UBS.Unbounded_String;
-      Tree_Ref : SHA256_Value;
+      Tree_Ref : SHA256_Value := Null_Sha256;
    end record;
 
    function Hash (Key : UBS.Unbounded_String) return Ada.Containers.Hash_Type is
@@ -34,29 +37,57 @@ procedure Main is
       Branches : Branch_Map.Map;
    end record;
 
-   procedure Load_Branches is
-      Result : Branch_Info;
+   procedure Set_Branch(Info : in out Branch_Info; Item : Branch) is
+      use Branch_Map;
+      Result_Cursor : Branch_Map.Cursor;
+   begin
+      Result_Cursor := Info.Branches.Find(Item.Name);
+      if Result_Cursor = Branch_Map.No_Element then
+         Info.Branches.Insert(Item.Name, Item);
+      else
+         Info.Branches.Replace_Element(Result_Cursor, Item);
+      end if;
+   end Set_Branch;
 
+   procedure Load_Branches(Result : out Branch_Info) is
       procedure Handler(Name : JSON.UTF8_String; Value : JSON.JSON_Value) is
          Branch_Result : Branch;
       begin
          Branch_Result.Name := UBS.To_Unbounded_String(Name);
          Branch_Result.Tree_Ref := Value.Get("tree_ref");
-         Result.Branches.Insert(UBS.To_Unbounded_String(Name), Branch_Result);
+         Set_Branch(Result, Branch_Result);
       end Handler;
 
       Branch_Json : JSON.JSON_Value;
-      Branch_Cursor : Branch_Map.Cursor;
    begin
       Branch_Json := JSON.Read(File_Operations.Load_File (Config.Branch_JSON_File), "");
       JSON.Map_JSON_Object (Val => Branch_Json.Get("branches"), CB => Handler'Access);
-      Branch_Cursor := Result.Branches.First;
+   end Load_Branches;
+
+   procedure Save_Branches(Info : Branch_Info) is
+      Result_JSON, Branch_JSON, Branch_Entry_JSON : JSON.JSON_Value;
+      Branch_Cursor : Branch_Map.Cursor;
+      Temp_Branch : Branch;
+      Data_File : TIO.File_Type;
+   begin
+      Branch_JSON := JSON.Create_Object;
+      Branch_Cursor := Info.Branches.First;
       while Branch_Map.Has_Element(Branch_Cursor) loop
-         TIO.Put_Line(UBS.To_String(Branch_Map.Element(Branch_Cursor).Name));
+         Temp_Branch := Branch_Map.Element(Branch_Cursor);
+         Branch_Entry_JSON := JSON.Create_Object;
+         Branch_Entry_JSON.Set_Field("name", UBS.To_String(Temp_Branch.Name));
+         Branch_Entry_JSON.Set_Field("tree_ref", Temp_Branch.Tree_Ref);
+         Branch_JSON.Set_Field(UBS.To_String(Temp_Branch.Name), Branch_Entry_JSON);
          Branch_Map.Next(Branch_Cursor);
       end loop;
+      Result_JSON := JSON.Create_Object;
+      Result_JSON.Set_Field("branches", Branch_JSON);
+      Result_JSON.Set_Field("head", JSON.JSON_Null);
 
-   end Load_Branches;
+      TIO.Create(Data_File, TIO.Out_File, Config.Branch_JSON_File);
+      Ada.Text_IO.Put(Data_File, Result_JSON.Write);
+      TIO.Close(Data_File);
+   end Save_Branches;
 
    procedure Setup_Project is
       procedure Create_Dir (Path : String) is
@@ -71,7 +102,16 @@ procedure Main is
       Create_Dir (Config.Temp_Dir);
    end Setup_Project;
 
+   Branch_Status : Branch_Info;
+   Test_Branch : Branch;
 begin
    Setup_Project;
-   Load_Branches;
+   begin
+      Load_Branches(Branch_Status);
+   exception when Ada.IO_Exceptions.Name_Error => null;
+   end;
+
+   Test_Branch.Name := UBS.To_Unbounded_String("test");
+   Set_Branch(Branch_Status, Test_Branch);
+   Save_Branches(Branch_Status);
 end Main;
