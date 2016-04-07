ecf46a0bf96f4676b2e3079469ccfd0523d9e36c
kyle kersey <kylelk@comcast>
2016-04-07 12:06:25 -0700

    changed data directory

--- src/config.ads
+++ src/config.ads
@@ -2,7 +2,7 @@ with GNAT.Directory_Operations;
 package Config is
    function Join(Path : String) return String is (GNAT.Directory_Operations.Format_Pathname(Path));
 
-   Data_Dir : constant String := ".note_data/";
+   Data_Dir : constant String := ".note_chain_data/";
 
    Object_Dir : constant String := Join(Data_Dir & "objects");
 
