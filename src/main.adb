f12d254bfe25debd9cd1cb3e08133e1c0962feac
kyle kersey <kylelk@comcast.net>
2016-04-12 08:43:34 -0700

    adding config command

--- src/main.adb
+++ src/main.adb
@@ -55,48 +55,50 @@ procedure Main is
       Execute_System ("vim " & Config.Temp_Note_File);
    end Edit_Note_Content;
 
-   procedure List_Branches(Status : Client.Client_Status) is
-      Cursor : Client.Branch_Map.Cursor;
+   procedure List_Branches (Status : Client.Client_Status) is
+      Cursor        : Client.Branch_Map.Cursor;
       Branch_Result : Client.Branch;
       use Client.Branch_Map;
       use UBS;
    begin
       Cursor := Status.Branch_Status.Branches.First;
       while Cursor /= Client.Branch_Map.No_Element loop
-         Branch_Result := Client.Branch_Map.Element(Cursor);
+         Branch_Result := Client.Branch_Map.Element (Cursor);
          if Branch_Result.Name = Status.Branch_Status.Head then
             TIO.Put ("* ");
          else
-            TIO.Put("  ");
+            TIO.Put ("  ");
          end if;
-         TIO.Put_Line(UBS.To_String(Branch_Result.Name));
-         Cursor := Client.Branch_Map.Next(Cursor);
+         TIO.Put_Line (UBS.To_String (Branch_Result.Name));
+         Cursor := Client.Branch_Map.Next (Cursor);
       end loop;
    end List_Branches;
 
-   procedure Display_Commit(Item : Client.Commit) is
+   procedure Display_Commit (Item : Client.Commit) is
    begin
-      TIO.Put_Line(Item.Object_Ref & " " &
-                     Ada.Calendar.Formatting.Image(Item.Created_At));
+      TIO.Put_Line
+        (Item.Object_Ref &
+         " " &
+         Ada.Calendar.Formatting.Image (Item.Created_At));
    end Display_Commit;
-   
-   procedure List_Notes(Status : Client.Client_Status) is
-      Next_Ref : Client.SHA256_Value;
+
+   procedure List_Notes (Status : Client.Client_Status) is
+      Next_Ref    : Client.SHA256_Value;
       Tree_Result : Client.Tree_Entry;
       Note_Result : Client.Note;
-      use client;
+      use Client;
    begin
       if Status.Head_Commit_Ref = Client.Empty_Hash_Ref then
-         TIO.Put_Line("tree is null");
+         TIO.Put_Line ("tree is null");
          return;
       end if;
-      
+
       Next_Ref := Status.Head_Commit.Tree_Ref;
-      while next_ref /= Client.Empty_Hash_Ref loop
-         Tree_Result := Client.Get_Tree_Entry(next_ref);
+      while Next_Ref /= Client.Empty_Hash_Ref loop
+         Tree_Result := Client.Get_Tree_Entry (Next_Ref);
          if Tree_Result.Entry_Type = Client.Type_Note then
-            Note_Result := Client.Get_Note(Tree_Result.Child_Ref);
-            TIO.Put_Line(Note_Result.Object_Ref);
+            Note_Result := Client.Get_Note (Tree_Result.Child_Ref);
+            TIO.Put_Line (Note_Result.Object_Ref);
          end if;
          Next_Ref := Tree_Result.Next_Ref;
       end loop;
@@ -112,14 +114,15 @@ procedure Main is
                   .Head, UBS.To_Unbounded_String
                   (CLI.Argument (3)));
                TIO.Put_Line ("created branch: " & CLI.Argument (3));
-               
+
             elsif CLI.Argument (2) = "checkout" then
-               Info.Checkout_Branch(UBS.To_Unbounded_String(CLI.Argument(3)));
-               TIO.Put_Line("changed to branch: " & CLI.Argument(3));
-               
+               Info.Checkout_Branch
+               (UBS.To_Unbounded_String (CLI.Argument (3)));
+               TIO.Put_Line ("changed to branch: " & CLI.Argument (3));
+
             elsif CLI.Argument (2) = "merge" then
                TIO.Put_Line ("TODO: merge branch");
-               
+
             elsif CLI.Argument (2) = "remove" then
                TIO.Put_Line ("TODO: remove");
             end if;
@@ -127,84 +130,108 @@ procedure Main is
          if CLI.Argument (2) = "head" then
             TIO.Put_Line ("head: " & UBS.To_String (Info.Branch_Status.Head));
          elsif CLI.Argument (2) = "list" then
-            List_Branches(Info);
+            List_Branches (Info);
          end if;
       else
-         List_Branches(Info);
+         List_Branches (Info);
       end if;
    end Cmd_Branch;
 
+   procedure Cmd_Config (Status : in out Client.Client_Status) is
+      pragma Unreferenced (Status);
+   begin
+      if CLI.Argument_Count = 2 then
+         if CLI.Argument (2) = "list" then
+            TIO.Put_Line ("TODO: list config items");
+         end if;
+      elsif CLI.Argument_Count = 3 then
+         if CLI.Argument (2) = "get" then
+            TIO.Put_Line ("TODO: get value of " & CLI.Argument (3));
+         end if;
+      elsif CLI.Argument_Count > 3 then
+         if CLI.Argument (2) = "set" then
+            TIO.Put_Line
+              ("TODO: set value of " &
+               CLI.Argument (3) &
+               "=" &
+               CLI.Argument (4));
+         end if;
+      end if;
+   end Cmd_Config;
+
    procedure Cmd_Note (Status : in out Client.Client_Status) is
-      Note_Item : Client.Note;
+      Note_Item  : Client.Note;
       Note_Entry : Client.Tree_Entry;
       New_Commit : Client.Commit;
    begin
       Note_Entry.Entry_Type := Client.Type_Note;
 
       if Status.Head_Commit_Ref /= Client.Empty_Hash_Ref then
-         Note_Entry.Next_Ref := Status.Head_Commit.Tree_Ref;
+         Note_Entry.Next_Ref   := Status.Head_Commit.Tree_Ref;
          New_Commit.Parent_Ref := Status.Head_Commit_Ref;
       end if;
 
       if CLI.Argument_Count > 1 then
          if CLI.Argument (2) = "new" then
             Edit_Note_Content;
-            Status.Create_Note(Note_Item);
-            Status.Save(Note_Item);
+            Status.Create_Note (Note_Item);
+            Status.Save (Note_Item);
 
             -- save tree entry for note
             Note_Entry.Child_Ref := Note_Item.Object_Ref;
-            Status.Save(Note_Entry);
+            Status.Save (Note_Entry);
 
             -- create new commit for changes
             New_Commit.Tree_Ref := Note_Entry.Object_Ref;
-            Status.Save(New_Commit);
+            Status.Save (New_Commit);
 
             -- update the head commit to point to the newest tree
-            Status.Set_Head(New_Commit);
+            Status.Set_Head (New_Commit);
 
-            TIO.Put_Line("created new note");
+            TIO.Put_Line ("created new note");
          end if;
 
          if CLI.Argument_Count > 2 then
-            if CLI.Argument(2) = "view" then
+            if CLI.Argument (2) = "view" then
                null;
-            elsif CLI.Argument(2) = "print" then
-               TIO.Put_Line(UBS.To_String(Client.Get_Note(CLI.Argument(3)).Note_Text));
+            elsif CLI.Argument (2) = "print" then
+               TIO.Put_Line
+                 (UBS.To_String
+                    (Client.Get_Note (CLI.Argument (3)).Note_Text));
             end if;
          end if;
-         
-         if CLI.Argument(2) = "list" then
-            List_Notes(Status);
+
+         if CLI.Argument (2) = "list" then
+            List_Notes (Status);
          end if;
       end if;
    end Cmd_Note;
 
-   procedure Cmd_Log(Status : in out Client.Client_Status) is
+   procedure Cmd_Log (Status : in out Client.Client_Status) is
       Next_Commit_Ref : Client.SHA256_Value;
-      Next_Commit : Client.Commit;
+      Next_Commit     : Client.Commit;
    begin
       if CLI.Argument_Count > 1 then
          null;
       else
          Next_Commit_Ref := Status.Head_Commit_Ref;
          while Next_Commit_Ref /= Client.Empty_Hash_Ref loop
-            Next_Commit := Client.Get_Commit(Next_Commit_Ref);
-            Display_Commit(Next_Commit);
+            Next_Commit := Client.Get_Commit (Next_Commit_Ref);
+            Display_Commit (Next_Commit);
             Next_Commit_Ref := Next_Commit.Parent_Ref;
          end loop;
       end if;
    end Cmd_Log;
 
-   procedure Cmd_Object(Status : Client.Client_Status) is
+   procedure Cmd_Object (Status : Client.Client_Status) is
       pragma Unreferenced (Status);
    begin
       if CLI.Argument_Count > 1 then
          if CLI.Argument_Count > 2 then
-            if CLI.Argument(2) = "type" then
-               TIO.Put_Line(Object_Store.Object_Type(CLI.Argument(3)));
-            elsif CLI.Argument(2) = "print" then
-               TIO.Put_Line(Object_Store.Read(CLI.Argument(3)));
+            if CLI.Argument (2) = "type" then
+               TIO.Put_Line (Object_Store.Object_Type (CLI.Argument (3)));
+            elsif CLI.Argument (2) = "print" then
+               TIO.Put_Line (Object_Store.Read (CLI.Argument (3)));
             end if;
          end if;
       end if;
@@ -212,7 +239,11 @@ procedure Main is
 
    procedure Display_Help is
       longest_name : Integer := 0;
-      procedure P(Run, Indent : Integer; Name : String; Help : String := "") is
+      procedure P
+        (Run, Indent : Integer;
+         Name        : String;
+         Help        : String := "")
+      is
          use Ada.Strings.Fixed;
       begin
          if Run = 1 then
@@ -223,38 +254,45 @@ procedure Main is
             if Indent = 0 then
                TIO.New_Line;
             end if;
-            TIO.Put_Line(
-            Indent*" " & 
-            Name & ((longest_name+4)-Name'Length-Indent)*" " & Help);
+            TIO.Put_Line
+              (Indent * " " &
+               Name &
+               ((longest_name + 4) - Name'Length - Indent) * " " &
+               Help);
          end if;
       end P;
    begin
-      TIO.Put_Line("usage:");
-      
-      for R in 1..2 loop
-         P(R, 0, "branch");
-         P(R, 2, "checkout <name>", "change into another branch");
-         P(R, 2, "head", "get the name of the curent branch");
-         P(R, 2, "list", "list all branches");
-         P(R, 2, "merge <name>", "pull the commits of a another branch info the current");
-         P(R, 2, "new <name>", "create a new branch from the current");
-         P(R, 2, "remove <name>", "delete a branch");
-         
-         P(R, 0, "help", "displays the usage options");
-         
-         P(R, 0, "log", "displays a list of commits in the current branch");
+      TIO.Put_Line ("usage:");
 
-         P(R, 0, "object");
-         P(R, 2, "print <sha256>", "show the content of a data object");
-         P(R, 2, "type <sha256>", "get the type of a data object");
+      for R in 1 .. 2 loop
+         P (R, 0, "branch");
+         P (R, 2, "checkout <name>", "change into another branch");
+         P (R, 2, "head", "get the name of the curent branch");
+         P (R, 2, "list", "list all branches");
+         P (R, 2, "merge <name>", "merges the commits of a another branch");
+         P (R, 2, "new <name>", "create a new branch from the current");
+         P (R, 2, "remove <name>", "delete a branch");
          
-         P(R, 0, "note"); 
-         P(R, 2, "list", "list notes in the current branch");
-         P(R, 2, "new", "create a new note");
-         P(R, 2, "print <sha256>", "display the text of a note");
-         P(R, 2, "view <sha256>", "view the content of a note in the editor");
+         P (R, 0, "config");
+         P (R, 2, "list", "list all config items");
+         P (R, 2, "get <key>", "gets a config item by a key");
+         P (R, 2, "set <key> <value>", "sets a config item");
 
-         P(R, 0, "version", "displays the current version number");
+         P (R, 0, "help", "displays the usage options");
+
+         P (R, 0, "log", "displays a list of commits in the current branch");
+
+         P (R, 0, "object");
+         P (R, 2, "print <sha256>", "show the content of a data object");
+         P (R, 2, "type <sha256>", "get the type of a data object");
+
+         P (R, 0, "note");
+         P (R, 2, "list", "list notes in the current branch");
+         P (R, 2, "new", "create a new note");
+         P (R, 2, "print <sha256>", "display the text of a note");
+         P (R, 2, "view <sha256>", "view the content of a note in the editor");
+
+         P (R, 0, "version", "displays the current version number");
       end loop;
    end Display_Help;
 
@@ -281,18 +319,20 @@ begin
    if CLI.Argument_Count >= 1 then
       if CLI.Argument (1) = "branch" then
          Cmd_Branch (Note_Client);
+      elsif CLI.Argument (1) = "config" then
+         Cmd_Config (Note_Client);
       elsif CLI.Argument (1) = "help" then
          Display_Help;
       elsif CLI.Argument (1) = "--help" then
          Display_Help;
       elsif CLI.Argument (1) = "note" then
          Cmd_Note (Note_Client);
-      elsif CLI.Argument(1) = "version" then
-         TIO.Put_Line(Config.Version);
-      elsif CLI.Argument(1) = "log" then
-         Cmd_Log(Note_Client);
-      elsif CLI.Argument(1) = "object" then
-         Cmd_Object(Note_Client);
+      elsif CLI.Argument (1) = "version" then
+         TIO.Put_Line (Config.Version);
+      elsif CLI.Argument (1) = "log" then
+         Cmd_Log (Note_Client);
+      elsif CLI.Argument (1) = "object" then
+         Cmd_Object (Note_Client);
       end if;
    else
       TIO.Put_Line ("call 'help' command for more infomation");
