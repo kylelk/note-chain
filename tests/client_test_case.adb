8a794bc79577ec4c2e1d620ee1c062c11dccc9c5
kyle kersey <kylelk@comcast.net>
2016-04-16 23:52:17 -0700

    adding client tests

--- tests/client_test_case.adb
+++ tests/client_test_case.adb
@@ -1,28 +1,49 @@
 with AUnit.Assertions; use AUnit.Assertions;
+with Ada.Text_IO;
+
 with Client;
-with AUnit.Test_Cases; use AUnit.Test_Cases;
+with Config;
 
 package body Client_Test_Case is
 
-   ----------
-   -- Name --
-   ----------
+   Test_Client : Client.Client_Status;
 
    overriding function Name
      (Test : Client_Test_Case)
       return Message_String
    is
+      pragma Unreferenced (Test);
    begin
       return Format("Client test");
    end Name;
 
-   --------------------
-   -- Register_Tests --
-   --------------------
+   procedure Test_Create_Branch(T : in out Test_Case'Class) is
+   begin
+      Set_Up(T);
+      Test_Client.Copy_Branch(Test_Client.Branch_Status.Head, +"test");
+   end Test_Create_Branch;
 
-   procedure Register_Tests (Test : in out Client_Test_Case) is
+   procedure Set_Up_Case(Test : in out Test_Case'Class) is
+      pragma Unreferenced (Test);
+      Default_Value : Client.Client_Status;
+      Default_Branch : Client.Branch;
+   begin
+      Test_Client := Default_Value;
+      Default_Branch.Name := Config.Default_Branch_Name;
+ --     Test_Client.Set_Branch (Default_Branch);
+      Test_Client.Checkout_Branch (Config.Default_Branch_Name);
+      Ada.Text_IO.Put_Line("hello world");
+   end Set_Up_Case;
+
+   procedure Tear_Down_Case(Test : in out Test_Case'Class) is
    begin
       null;
+   end Tear_Down_Case;
+
+   procedure Register_Tests (Test : in out Client_Test_Case) is
+      use AUnit.Test_Cases.Registration;
+   begin
+      Register_Routine(Test, Test_Create_Branch'Access, "Test create branch");
    end Register_Tests;
 
 end Client_Test_Case;
--- tests/client_test_case.ads
+++ tests/client_test_case.ads
@@ -1,12 +1,22 @@
 with AUnit; use AUnit;
-with AUnit.Test_Cases;
+with AUnit.Test_Cases; use AUnit.Test_Cases;
+with Ada.Strings.Unbounded;
 
 package Client_Test_Case is
    type Client_Test_Case is new AUnit.Test_Cases.Test_Case with null record;
+   package UBS renames Ada.Strings.Unbounded;
 
+   function "+"(S : String) return UBS.Unbounded_String
+   is (UBS.To_Unbounded_String(S));
 
    overriding
    function Name (Test : Client_Test_Case) return Message_String;
+
+   procedure Test_Create_Branch (T : in out Test_Case'Class);
+
+   procedure Set_Up_Case(Test : in out Test_Case'Class);
+
+   procedure Tear_Down_Case(Test : in out Test_Case'Class);
 
    procedure Register_Tests( Test : in out Client_Test_Case );
 
