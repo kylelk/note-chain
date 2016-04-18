0b2185ca9b9ab6e2adb864763a1031469fa78e99
kyle kersey <kylelk@comcast.net>
2016-04-18 09:01:47 -0700

    fixed branch name validator

--- src/client.adb
+++ src/client.adb
@@ -416,13 +416,17 @@ package body Client is
 
    function Valid_Branch_Name(Name : String) return Boolean is
       package Pat renames GNAT.Regpat;
-      Valid_Pattern : constant String := "([A-Za-z0-9_\-\+\(\)]+|\.\.)\&";
+      Valid_Pattern : constant String := "^([A-Za-z0-9_\-\+\(\)]+)$";
    begin
       if Name'Length = 40 then
          return False;
-      else
-         return Pat.Match(Valid_Pattern, Name);
       end if;
+
+      if Pat.Match("\.\.", Name) then
+	 return False;
+      end if;
+
+      return Pat.Match(Valid_Pattern, Name);
    end Valid_Branch_Name;
 
 
