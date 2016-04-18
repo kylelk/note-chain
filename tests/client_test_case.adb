45d42fffc4ba8edfdc3aafd2fbaa4e482f4b3a9e
kyle kersey <kylelk@comcast.net>
2016-04-18 09:19:00 -0700

    added more branch name tests

--- tests/client_test_case.adb
+++ tests/client_test_case.adb
@@ -4,35 +4,45 @@ with Client;
 
 package body Client_Test_Case is
 
-   overriding function Name
-     (Test : Client_Test_Case)
-      return Message_String
-   is
+   overriding function Name (Test : Client_Test_Case) return Message_String is
       pragma Unreferenced (Test);
    begin
-      return Format("Client test");
+      return Format ("Client test");
    end Name;
 
-   procedure Test_Create_Branch(T : in out Test_Case'Class) is
+   procedure Test_Create_Branch (T : in out Test_Case'Class) is
       pragma Unreferenced (T);
       Test_Client : Client.Client_Status;
       Test_Branch : constant Client.Branch := (+"test", Client.Empty_Hash_Ref);
    begin
-      Test_Client.Set_Branch(Test_Branch);
-      Assert(Test_Client.Branch_Exists(+"test"), "Branch creation failed");
+      Test_Client.Set_Branch (Test_Branch);
+      Assert (Test_Client.Branch_Exists (+"test"), "Branch creation failed");
    end Test_Create_Branch;
 
    procedure Test_Valid_Branch_Name (T : in out Test_Case'Class) is
-      pragma Unreferenced(T);
+      pragma Unreferenced (T);
    begin
-      Assert(Client.Valid_Branch_Name("test"), "failed branch name");
+      Assert (Client.Valid_Branch_Name ("test"), "failed branch name");
+      Assert
+        (not Client.Valid_Branch_Name ("test..master"),
+         "failed branch name");
+      Assert
+        (not Client.Valid_Branch_Name
+           ("2aae6c35c94fcfb415dbe95f408b9ce91ee846ed"),
+         "failed branch name");
+      Assert
+        (not Client.Valid_Branch_Name ("Contains Space"),
+         "failed branch name");
    end Test_Valid_Branch_Name;
 
    procedure Register_Tests (Test : in out Client_Test_Case) is
       use AUnit.Test_Cases.Registration;
    begin
-      Register_Routine(Test, Test_Create_Branch'Access, "Test create branch");
-      Register_Routine(Test, Test_Valid_Branch_Name'Access, "Test branch branch name validation");
+      Register_Routine (Test, Test_Create_Branch'Access, "Test create branch");
+      Register_Routine
+        (Test,
+         Test_Valid_Branch_Name'Access,
+         "Test branch name validation");
    end Register_Tests;
 
 end Client_Test_Case;
